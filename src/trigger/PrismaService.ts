781903f4a835caddc0bbd7eef6796e13e18a98ae
Tapu45 <rameswarpanda891@gmail.com>
2025-09-20 16:42:15 +0530

    fuck u2

--- src/trigger/PrismaService.ts
+++ src/trigger/PrismaService.ts
@@ -1,20 +1,17 @@
 import { PrismaClient } from "../generated/prisma/index";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 let prisma: PrismaClient | null = null;
 
 export async function getPrismaClient() {
     if (!prisma) {
-        const databaseUrl = process.env.DATABASE_URL_ACCELERATE || process.env.DATABASE_URL;
-
         prisma = new PrismaClient({
             log: ['error'],
             datasources: {
                 db: {
-                    url: databaseUrl
+                    url: process.env.DATABASE_URL
                 }
             }
-        }).$extends(withAccelerate());
+        });
     }
     return prisma;
 }
